7ed38fe728075ccb2e151014f77c761a8fab78c4
hafihaf123 <misobuchta@gmail.com>
2024-01-30 21:55:42 +0100

    create file morse.c

---
+++ morse.c
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  morse.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/30/24 21:49:10
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (Michal Buchman), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdlib.h>
+
