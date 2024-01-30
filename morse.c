1eb1796d5d940afef02666d6a134ff8ec68142a2
hafihaf123 <misobuchta@gmail.com>
2024-01-30 23:12:33 +0100

    added struct and getMorse()

--- morse.c
+++ morse.c
@@ -17,4 +17,35 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
+struct MorseMap {
+	char symbol;
+	char* morseCode
+};
+
+struct MorseMap map[] = {
+	{'A', ".-"}
+};
+
+char* getMorse(char c) {
+	for (int i=0; i < sizeof(map) / sizeof(map[0]); i++) {
+		if (toupper(c) == map[i].symbol) {
+			char* res = malloc(strlen(map[i].morseCode) + 1);
+			strcpy(res, map[i].morseCode);
+			return res;
+		}
+	}
+	return strdup("");
+}
+
+int main () {
+	printf("enter the expression:");
+	char text[20];
+	fgets(text, sizeof(text), stdin);
+	printf("%s", text);
+
+	return 0;
+}
